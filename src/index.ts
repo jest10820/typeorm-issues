af4cfa8e6b14f4b13bdf7ff5ebcdfa87f5ef9e58
Jordi Esteban <jest10820@gmail.com>
2019-01-22 04:50:09 +0100

    Partial commit

--- src/index.ts
+++ src/index.ts
@@ -4,65 +4,63 @@ import {
   createConnection,
   Entity,
   getManager,
-  ManyToOne,
-  PrimaryGeneratedColumn,
   Index,
+  ManyToOne,
   OneToMany,
+  PrimaryGeneratedColumn,
 } from 'typeorm';
 
-@Entity({ name: 'user' })
+@Entity('users')
 export class User {
-	@PrimaryGeneratedColumn('uuid')
-	id: string;
-}
-
-@Entity({ name: 'party' })
-export class Party {
-	@PrimaryGeneratedColumn('uuid')
-	id: string;
-
-  @OneToMany(type => Guests, guest => guest.party, {
-    nullable: true,
-    cascade: true,
-    onDelete: 'CASCADE',
-    onUpdate: 'CASCADE',
-  })
-  guests: User[] | null;
-}
-
-@Entity({ name: 'guests' })
-@Index((relation: Guests) => [relation.user, relation.party], { unique: true })
-export class Guests {
   @PrimaryGeneratedColumn('uuid')
   public id: string;
 
-  @ManyToOne(type => User, user => user.id, {
-    nullable: false,
-    primary: true,
-  })
-  user: User;
+  @OneToMany(type => Guest, guest => guest.user)
+  public parties: Guest[];
+}
+@Entity('parties')
+export class Party {
+  @PrimaryGeneratedColumn('uuid')
+  public id: string;
 
-  @ManyToOne(type => Party, party => party.id, {
-    nullable: false,
-    primary: true,
+  @OneToMany(type => Guest, guest => guest.party, {
+    cascade: true,
+    eager: true,
+    // I'm not sure if I need this
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
   })
-  party: Party;
+  public users: Guest[];
+}
+
+@Entity('guests')
+@Index(['user', 'party'], { unique: true })
+export class Guest {
+  @PrimaryGeneratedColumn('uuid')
+  public readonly id: number;
+
+  @ManyToOne(type => User, user => user.parties, {
+    eager: true,
+    // I'm not sure if I need this
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  public user: User;
+
+  @ManyToOne(type => Party, party => party.users)
+  public party: Party;
 
   @Column({
-    default: false
+    default: false,
   })
-  confirmed: boolean;
+  public confirmed: boolean;
 }
 
 const run = async () => {
   try {
     await createConnection({
       dropSchema: true,
-      entities: [
-        User,
-        Party,
-        Guests
-      ],
+      entities: [User, Party, Guest],
       logging: ['error', 'query'],
       synchronize: true,
       type: 'postgres',
@@ -77,23 +75,65 @@ const run = async () => {
     const user3 = new User();
     await manager.save(user3);
 
-    const party = new Party();
-    party.guests = [user1, user3];
+    const party1 = new Party();
+    const guest1 = new Guest();
+    // guest1.user = user1;
+    guest1.user = {
+      id: user1.id,
+    } as any;
+    const guest2 = new Guest();
+    guest2.user = user3;
+    party1.users = [guest1, guest2];
+    const savedParty = await manager.save(party1);
 
+    let users = await manager
+      .getRepository(User)
+      .findOne(user1.id, { relations: ['parties'] });
+    // tslint:disable-next-line:no-console
+    console.log('User relations');
+    // tslint:disable-next-line:no-console
+    console.log(users);
+    let party = await manager.getRepository(Party).findOne(savedParty.id);
+    // tslint:disable-next-line:no-console
+    console.log('Parties');
+    // tslint:disable-next-line:no-console
     console.log(party);
-    // I would expect this to create an entry
-    // in Party and two entries in Guests
-    const myParty = await manager.save(party);
-    console.log(myParty);
+    let relation = await manager
+      .getRepository(Guest)
+      .find({ where: { party: { id: savedParty.id } } });
+    // tslint:disable-next-line:no-console
+    console.log('Relations');
+    // tslint:disable-next-line:no-console
+    console.log(relation);
 
-    const myPartyGuests = await manager.findOne(Party, myParty.id, {
-      relations: ['guests']
-    });
-    console.log(myPartyGuests);
+    const dbParty = await manager.getRepository(Party).findOne(savedParty.id);
+    dbParty.users = [guest2];
+    const newSave = await manager.save(dbParty);
+
+    users = await manager
+      .getRepository(User)
+      .findOne(user1.id, { relations: ['parties'] });
+    // tslint:disable-next-line:no-console
+    console.log('User relations');
+    // tslint:disable-next-line:no-console
+    console.log(users);
+    party = await manager.getRepository(Party).findOne(newSave.id);
+    // tslint:disable-next-line:no-console
+    console.log('Parties');
+    // tslint:disable-next-line:no-console
+    console.log(party);
+    relation = await manager
+      .getRepository(Guest)
+      .find({ where: { party: { id: newSave.id } } });
+    // tslint:disable-next-line:no-console
+    console.log('Relations');
+    // tslint:disable-next-line:no-console
+    console.log(relation);
   } catch (err) {
+    // tslint:disable-next-line:no-console
     console.log(`Error ${err}`);
     process.exit(1);
   }
-}
+};
 
 run();
